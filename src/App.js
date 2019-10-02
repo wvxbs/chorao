9b8d387e9539483b25b6746f0de7f7dc549cea6f
wvxbs <gabriel.ferreira7854@gmail.com>
2019-10-02 12:50:28 -0300

    app start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Style.sass';
+import { BrowserRouter, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Route  />
+    </BrowserRouter>
+  )
+}
+
+export default App;
---
+++ src/sign/Layout.js
@@ -0,0 +1,7 @@
+const Layout = props => {
+    return (
+        <div>
+            <h1>Bem vindx</h1>
+        </div>
+    )
+}
