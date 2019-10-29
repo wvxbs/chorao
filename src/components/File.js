0a1262f9716ec3092ff61c3669fe0a7534da7b20
wvxbs <gabriel.ferreira7854@gmail.com>
2019-10-29 19:40:49 -0300

    file delete function done

--- src/components/File.js
+++ src/components/File.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import axios from 'axios'
+import Req from '../Req'
 
 const File = props => {
 
-    const handleDownload = () => {
+    const handleDownload = file => {
         axios({
-            method : "DELETE"
-            ur
+            method: 'get',
+            url: Req.ListAll + "?filename=" + file,
+        })
+    }
+
+    const handleDelete = file => {
+        axios({
+            method: 'delete',
+            url: Req.ListAll + "?filename=" + file,
+        }).then(()=> {
+            window.location.reload();
         })
     }
 
@@ -14,14 +24,13 @@ const File = props => {
         <div className="card">
             <header className="card-header">
                 <p className="card-header-title">
-                    .
                 </p>
-                <a className="card-header-icon has-text-black" aria-label="more options">
+                <a className="card-header-icon has-text-black" aria-label="more options" onClick={() => handleDownload(props.name)}>
                     <span className="icon">
                         <i className="fas fa-cloud-download-alt"></i>
                     </span>
                 </a>
-                <a className="card-header-icon has-text-black" aria-label="more options">
+                <a className="card-header-icon has-text-black" aria-label="more options" onClick={() => handleDelete(props.name)}>
                     <span className="icon">
                         <i className="far fa-times-circle has-text-danger"></i>
                     </span>
